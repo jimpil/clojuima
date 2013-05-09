1f652be121817513bf79c42ef39670576b8869d9
Dimitrios Piliouras <jimpil1985@gmail.com>
2013-05-09 17:45:50 +0100

    dependencies updated

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject clojuima "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"] 
+                 [org.apache.uima/uimaj-core "2.4.0"]
+                 [org.uimafit/uimafit "1.4.0"]
+                 [org.apache.uima/Tagger "2.3.1"]
+                 [org.apache.uima/WhitespaceTokenizer "2.3.1"]]
+                 
+   :java-source-paths ["src/clojuima/java"]              
+                 )
